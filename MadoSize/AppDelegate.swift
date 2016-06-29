4009abe39bf67e0c65081af3607733730eaa65f9
Shad Sharma <shadanan@gmail.com>
2016-06-28 22:36:25 -0500

    Initial Commit

---
+++ MadoSize/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MadoSize
+//
+//  Created by Shad Sharma on 6/28/16.
+//  Copyright © 2016 Shad Sharma. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MadoSize/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MadoSize
+//
+//  Created by Shad Sharma on 6/28/16.
+//  Copyright © 2016 Shad Sharma. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
